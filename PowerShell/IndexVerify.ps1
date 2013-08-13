42b1f1b024a2fca82d29ac653859c66c443baf70
elopinto <e.e.lopinto@gmail.com>
2013-08-13 19:49:59 -0400

    made pshell scripts more readable

--- PowerShell/IndexVerify.ps1
+++ PowerShell/IndexVerify.ps1
@@ -1,9 +1,10 @@
-﻿param ($indexurl = $(throw "sitemap file path required"))
+﻿param ($indexurl = $(throw "sitemap file path required"),
+	$savefolder = $(throw "save folder path required"))
 
-	
+New-Item -Path $savefolder -ItemType Directory
+
 [xml]$index = (Invoke-WebRequest $indexurl).content.trimstart("ï»¿")
 $locnodes = $index.sitemapindex.sitemap.loc.trimend()
-
 
 function checker
 {
@@ -16,20 +17,28 @@ function checker
 	{
 		try
 		{
-			$page = Invoke-WebRequest $node -MaximumRedirection 0 -ErrorAction:SilentlyContinue
+			$page = Invoke-WebRequest $node -MaximumRedirection 0 `
+				-ErrorAction:SilentlyContinue
 			$statuscode = $page.StatusCode
-			$canonical = ($page.parsedHTML.getElementsByTagName("link") | Where-Object -property rel -eq -value canonical).href
+			$link = $page.parsedHTML.getElementsByTagName("link") |
+				Where-Object -property rel -value canonical -EQ
+			$canonical = $link.href
 			$iscanonical = $canonical -eq $node
-			[PsCustomObject]@{"Num"=$num; "URL"=$node; "Status Code"=$statuscode; "Canonical"=$iscanonical; "Canonical URL"=$canonical}
+			[PsCustomObject]@{"Num"=$num; "URL"=$node; "Status Code"=$statuscode;
+				"Canonical"=$iscanonical; "Canonical URL"=$canonical}
 		}
 		catch
 		{
 			$result = $error[0].exception.response.statuscode.value__
 			$iscanonical = "N/A"
-			[PsCustomObject]@{"Num"=$num; "URL"=$node; "Status Code"=$result; "Canonical"=$iscanonical; "Canonical URL"="N/A"}
+			[PsCustomObject]@{"Num"=$num; "URL"=$node; "Status Code"=$result;
+				"Canonical"=$iscanonical; "Canonical URL"="N/A"}
 		}
 		[int]$completion = $num/$mapnodes.length*100
-		Write-Progress -activity "Getting status codes of sitemap URLs and entering them in CSV" -status "Progress:" -currentoperation $completion% -percentcomplete $completion
+		Write-Progress `
+			-activity "Getting status codes of sitemap URLs and entering them in CSV" `
+			-status "Progress:" -currentoperation $completion% `
+			-percentcomplete $completion
 		$num += 1
 	}
 }
@@ -38,5 +47,6 @@ foreach ($node in $locnodes)
 {
 	[xml]$sitemap = (Invoke-WebRequest $node).content.trimstart("ï»¿")
 	$mapname = $node.split('/')[-1] + ".csv"
-	checker $sitemap | Export-CSV $mapname -NoTypeInformation
+	$savefile = "$savefolder\$mapname"
+	checker $sitemap | Export-CSV $savefile -NoTypeInformation
 }
--- PowerShell/SitemapVerify.ps1
+++ PowerShell/SitemapVerify.ps1
@@ -13,21 +13,28 @@ function checker
 	{
 		try
 		{
-			$page = Invoke-WebRequest $node -MaximumRedirection 0 -ErrorAction:SilentlyContinue
+			$page = Invoke-WebRequest $node -MaximumRedirection 0 `
+				-ErrorAction:SilentlyContinue
 			$statuscode = $page.StatusCode
-			$links = $page.parsedHTML.getElementsByTagName("link") | Where-Object -property rel -value canonical -EQ
+			$links = $page.parsedHTML.getElementsByTagName("link") |
+				Where-Object -property rel -value canonical -EQ
 			$canonical = $links.href
 			$iscanonical = $canonical -eq $node
-			[PsCustomObject]@{"Num"=$num; "URL"=$node; "Status Code"=$statuscode; "Canonical"=$iscanonical; "Canonical URL"=$canonical}
+			[PsCustomObject]@{"Num"=$num; "URL"=$node; "Status Code"=$statuscode;
+				"Canonical"=$iscanonical; "Canonical URL"=$canonical}
 		}
 		catch
 		{
 			$result = $error[0].exception.response.statuscode.value__
 			$iscanonical = "N/A"
-			[PsCustomObject]@{"Num"=$num; "URL"=$node; "Status Code"=$result; "Canonical"=$iscanonical; "Canonical URL"="N/A"}
+			[PsCustomObject]@{"Num"=$num; "URL"=$node; "Status Code"=$result;
+				"Canonical"=$iscanonical; "Canonical URL"="N/A"}
 		}
 		[int]$completion = $num/$locnodes.length*100
-		Write-Progress -activity "Getting status codes of sitemap URLs and entering them in CSV" -status "Progress:" -currentoperation $completion% -percentcomplete $completion
+		Write-Progress `
+			-activity "Getting status codes of sitemap URLs and entering them in CSV" `
+			-status "Progress:" -currentoperation $completion% `
+			-percentcomplete $completion
 		$num += 1
 	}
 }

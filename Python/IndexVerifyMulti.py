2222708af0380a17503c5a3c052a9af4e67e3896
elopinto <e.e.lopinto@gmail.com>
2013-08-27 10:23:09 -0400

    changed request encoding back to ascii as utf-8 prevents lxml from parsing if page has BOM.

--- Python/IndexVerifyMulti.py
+++ Python/IndexVerifyMulti.py
@@ -17,14 +17,14 @@ class MapChecker(object):
 
     def get_map(self, url):
         sitemap_page = requests.get(url).text
-        sitemap_page = sitemap_page.encode('utf_8', 'ignore')
+        sitemap_page = sitemap_page.encode('ascii', 'ignore')
         sitemap = etree.fromstring(sitemap_page)
         map_nodes = "{%s}loc" % sitemap.nsmap[None]
         sitemap_urls = [url.text.strip() for url in sitemap.iter(map_nodes)]
         return sitemap_urls
 
     def check_canonical(self, request):
-        source = request.text.encode('utf_8', 'ignore')
+        source = request.text.encode('ascii', 'ignore')
         head = html.fromstring(source).head
         href = ''
         for item in head.iter('link'):

f5b00f7c5d271cec7e9230d3b8a9e45a4bff08e4
elopinto <e.e.lopinto@gmail.com>
2014-04-29 22:58:25 -0400

    Improved PowerShell scripts: Replaced foreach loops in  Check-Sitemap functions with pipline input to reduce memory consumption.

--- PowerShell/IndexVerify.ps1
+++ PowerShell/IndexVerify.ps1
@@ -6,51 +6,48 @@
     )
 
 # Create folder to save CSVs. Program produces 1 CSV for each sitemap.
-New-Item -Path $savefolder -ItemType Directory
+New-Item -Path $Target -ItemType Directory
 
 # Download and parse XML sitemap. Trim any leading BOM characters or trailing
 # spaces, and make list of sitemaps.
 [xml]$index = (Invoke-WebRequest $Indexurl).Content.TrimStart("ï»¿")
-$locnodes = $index.sitemapindex.sitemap.loc.TrimEnd()
+$mapnodes = $index.sitemapindex.sitemap.loc.TrimEnd()
 
 # Function: get status codes and canonical tags for URLs in a sitemap.
 Function Check-Sitemap
 {
-    param($sitemap)
-    
-    # create list of URLs in sitemap.
-    $mapnodes = $sitemap.urlset.url.loc.TrimEnd()
-    $num = 1
-    
-    ForEach ($node in $mapnodes) {
-        # try to download webpage and parse HTML to check canonical tag.
-        # return status code, and canonical info.
+    Begin {
+        $num = 1
+    }
+    Process {
+        # try to download page and get status code and canonical tag
+        $url = $_
         Try {
-            $page = Invoke-WebRequest $node -MaximumRedirection 0 `
+            $page = Invoke-WebRequest $url -MaximumRedirection 0 `
                 -ErrorAction:SilentlyContinue
             $statuscode = $page.StatusCode
-            $link = $page.ParsedHtml.getElementsByTagName("link") |
+            $links = $page.ParsedHtml.getElementsByTagName("link") |
                 Where-Object -Property rel -EQ -Value canonical
-            $canonical = $link.href
-            $iscanonical = $canonical -eq $node
-            [PsCustomObject]@{"Num"=$num; "URL"=$node; "Status Code"=$statuscode;
+            $canonical = $links.href
+            $iscanonical = $canonical -eq $url
+            [PsCustomObject]@{"Num"=$num; "URL"=$url; "Status Code"=$statuscode;
                 "Canonical"=$iscanonical; "Canonical URL"=$canonical}
         }
         # If request fails, return error info.
         Catch [System.Net.WebException] {
             $result = $error[0].Exception.Response.StatusCode.Value__
             $iscanonical = "N/A"
-            [PsCustomObject]@{"Num"=$num; "URL"=$node; "Status Code"=$result;
+            [PsCustomObject]@{"Num"=$num; "URL"=$url; "Status Code"=$result;
                 "Canonical"=$iscanonical; "Canonical URL"="N/A"}
         }
         Catch [System.Management.Automation.RuntimeException] {
-            $result = (Invoke-WebRequest $node -MaximumRedirection 0 `
+            $result = (Invoke-WebRequest $url -MaximumRedirection 0 `
                 -ErrorAction:SilentlyContinue).StatusCode
             $iscanonical = "N/A"
-            [PsCustomObject]@{"Num"=$num; "URL"=$node; "Status Code"=$result;
+            [PsCustomObject]@{"Num"=$num; "URL"=$url; "Status Code"=$result;
                 "Canonical"=$iscanonical; "Canonical URL"="HMTL error"}
         }
-        [int]$completion = $num/$mapnodes.length*100
+        [int]$completion = $num / $locnodes.length * 100
         Write-Progress `
             -Activity "Getting status codes of sitemap URLs and entering them in CSV" `
             -Status "Progress:" -CurrentOperation $completion% `
@@ -61,9 +58,10 @@ Function Check-Sitemap
 
 # Check status codes and canonical tags of URLs for each sitemap. Save results
 # in CSV file in folder created at beginning of program.
-ForEach ($node in $locnodes) {
-    [xml]$sitemap = (Invoke-WebRequest $node).Content.TrimStart("ï»¿")
-    $mapname = $node.Split('/')[-1] + ".csv"
-    $savefile = "$savefolder\$mapname"
-    Check-Sitemap $sitemap | Export-CSV $savefile -NoTypeInformation
+$mapnodes | ForEach-Object {
+    [xml]$sitemap = (Invoke-WebRequest $_).Content.TrimStart("ï»¿");
+    $locnodes = $sitemap.urlset.url.loc.TrimEnd();
+    $mapname = $_.Split('/')[-1] + ".csv";
+    $savefile = "$Target\$mapname";
+    $locnodes | Check-Sitemap | Export-CSV $savefile -NoTypeInformation
 }
--- PowerShell/SitemapVerifyLocal.ps1
+++ PowerShell/SitemapVerifyLocal.ps1
@@ -11,36 +11,38 @@ $locnodes = $sitemap.urlset.url.loc.TrimEnd()
 # Function: For each URL in sitemap, return status code and canonical tag.
 Function Check-Sitemap
 {
-    $num = 1
-    
-    ForEach ($node in $locnodes) {
+    Begin {
+        $num = 1
+    }
+    Process {
         # try to download page and get status code and canonical tag
+        $url = $_
         Try {
-            $page = Invoke-WebRequest $node -MaximumRedirection 0 `
+            $page = Invoke-WebRequest $url -MaximumRedirection 0 `
                 -ErrorAction:SilentlyContinue
             $statuscode = $page.StatusCode
             $links = $page.ParsedHtml.getElementsByTagName("link") |
                 Where-Object -Property rel -EQ -Value canonical
             $canonical = $links.href
-            $iscanonical = $canonical -eq $node
-            [PsCustomObject]@{"Num"=$num; "URL"=$node; "Status Code"=$statuscode;
+            $iscanonical = $canonical -eq $url
+            [PsCustomObject]@{"Num"=$num; "URL"=$url; "Status Code"=$statuscode;
                 "Canonical"=$iscanonical; "Canonical URL"=$canonical}
         }
         # If request fails, return error info.
         Catch [System.Net.WebException] {
             $result = $error[0].Exception.Response.StatusCode.Value__
             $iscanonical = "N/A"
-            [PsCustomObject]@{"Num"=$num; "URL"=$node; "Status Code"=$result;
+            [PsCustomObject]@{"Num"=$num; "URL"=$url; "Status Code"=$result;
                 "Canonical"=$iscanonical; "Canonical URL"="N/A"}
         }
         Catch [System.Management.Automation.RuntimeException] {
-            $result = (Invoke-WebRequest $node -MaximumRedirection 0 `
+            $result = (Invoke-WebRequest $url -MaximumRedirection 0 `
                 -ErrorAction:SilentlyContinue).StatusCode
             $iscanonical = "N/A"
-            [PsCustomObject]@{"Num"=$num; "URL"=$node; "Status Code"=$result;
+            [PsCustomObject]@{"Num"=$num; "URL"=$url; "Status Code"=$result;
                 "Canonical"=$iscanonical; "Canonical URL"="HMTL error"}
         }
-        [int]$completion = $num/$locnodes.length*100
+        [int]$completion = $num / $locnodes.length * 100
         Write-Progress `
             -Activity "Getting status codes of sitemap URLs and entering them in CSV" `
             -Status "Progress:" -CurrentOperation $completion% `
@@ -50,4 +52,4 @@ Function Check-Sitemap
 }
 
 # Run program, save results in CSV file.
-Check-Sitemap | Export-CSV $target -NoTypeInformation
+$locnodes | Check-Sitemap | Export-CSV $target -NoTypeInformation
